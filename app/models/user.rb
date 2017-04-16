473f8f74953e5c792f1cb8d421382072eedf6369
Beth Cummings <bethios@gmail.com>
2017-04-15 20:50:27 -0500

    heroku fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable #, :confirmable
 
   before_save { self.email = email.downcase }
-  before_save { self.role ||= :standard }
+  after_initialize { self.role ||= :standard }
 
   enum role: [:standard, :admin, :premium]
 
