fdf3b0c82615e223c14de88ac48fea2bb00dada6
Beth Cummings <bethios@gmail.com>
2017-04-18 13:04:26 -0500

    layout fix

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    :tls => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :authentication => :login,
-    :user_name => "bethios@gmail.com",
-    :password => ""
+  address:        'smtp.sendgrid.net',
+  port:           '2525',
+  authentication: :plain,
+  user_name:      ENV['SENDGRID_USERNAME'],
+  password:       ENV['SENDGRID_PASSWORD'],
+  domain:         'heroku.com',
+  enable_starttls_auto: true
 }
