bec05b432daad0283fcd247d8811cdb50a8c6dd7
Beth Cummings <bethios@gmail.com>
2017-04-20 15:47:50 -0500

    added markdown cheatsheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'charges/create'
-
   devise_for :users
 
-  resources :charges, only: [:new, :create, :delete]
+  #get 'charges/create'
   get '/charges', to: 'charges#delete', as: :downgrade
+
+  resources :charges, only: [:new, :create, :delete]
 
   resources :wikis
 
