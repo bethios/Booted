3abd5d507d1ee976dc560a6b743949a1f38b2bef
Beth Cummings <bethios@gmail.com>
2017-04-22 14:54:31 -0500

    css fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  #get 'charges/create'
   get '/charges', to: 'charges#delete', as: :downgrade
 
   resources :charges, only: [:new, :create, :delete]
